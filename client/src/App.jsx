704c97ca757eca3c37e91049eb0755d7ffb94f78
abhi <abhi.nolyfe@gmail.com>
2023-07-25 02:25:02 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter } from "react-router-dom";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <div>
+        <h1>hola</h1>
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
